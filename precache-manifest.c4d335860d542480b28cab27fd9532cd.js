a91a5f31a42b06600d2b8aa4772be6d1d6ae1925
alio1991 <alioli_15@hotmail.com>
2020-03-24 13:04:43 +0100

    Updates

---
+++ precache-manifest.c4d335860d542480b28cab27fd9532cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bca1f2c473908336cb65fb78678ee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e56656c8ba4f51ef9704",
+    "url": "/static/css/main.52139d5b.chunk.css"
+  },
+  {
+    "revision": "bbb4b6b5e6ce8d28a9cd",
+    "url": "/static/js/2.0d8132f0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0d8132f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56656c8ba4f51ef9704",
+    "url": "/static/js/main.0d1c0603.chunk.js"
+  },
+  {
+    "revision": "a3449944639dfeb87e1a",
+    "url": "/static/js/runtime-main.f8c53427.js"
+  }
+]);
