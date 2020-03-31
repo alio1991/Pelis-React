ef37321091cbaa72832e44152fce95c99dd7a079
alio1991 <alioli_15@hotmail.com>
2020-03-31 11:54:33 +0200

    App.js transated to functional component

--- src/App.js
+++ src/App.js
@@ -4,40 +4,16 @@ import InputSearch from './components/input-search/input-search.jsx';
 import MovieList from './components/movie-list/movie-list.jsx';
 
 
-class  App extends React.Component {
+function App() {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      list: [],
-      favorites: this.loadFromLocalStorage('favorites') || []
-    }
-    this.getMovies = this.getMovies.bind(this);
-    this.cardSelected = this.cardSelected.bind(this);
-
+  const  loadFromLocalStorage = (name) => { 
+    return JSON.parse(localStorage.getItem(name));
   }
 
-  render() { 
-    const { getMovies } = this;
-    const { cardSelected } = this;
-
-    return (
-      <div className="App">
-          <InputSearch buttonName={'busca'} actionToPerform={ getMovies }></InputSearch>
-          <main>
-            <aside className="aside">
-              <MovieList list={this.state.favorites} cardSelected={ cardSelected } class="favorite" ></MovieList>
-            </aside>
-            <section className="section">
-              <MovieList list={this.state.list}  cardSelected={ cardSelected } class="normal" ></MovieList>
-            </section>
-          </main>
-      </div>
-    );
-  }
-
-
-  getMovies(inputValue){    
+  const [list, setList] = React.useState([]);
+  const [favorites, setFavorites] = React.useState(loadFromLocalStorage('favorites') || []);
+  
+  const getMovies = (inputValue) => { 
     const page = 1;
     const type = 'movie'; // movie, series, episode
     fetch(`https://www.omdbapi.com/?s=${inputValue}&plot=full&apikey=e477ed6a&page=${page}&type=${type}`)
@@ -45,42 +21,39 @@ class  App extends React.Component {
       return response.json();
     }).then(result => {
       if(result.Search && result.Search.length > 0){
-        this.setState({list: []});  
-        this.setState({list: result.Search});    
+        setList([]);  
+        setList(result.Search);    
       }else{
-        this.setState({list: []});    
+        setList([]);    
       }
     });
   }
 
-  cardSelected(ev){
+  const cardSelected = (ev) => { 
     const id = ev.currentTarget.id;
-    const newFav = this.searchById(id,this.state.list);
-    const isAlreadyInFavs = this.searchById(id,this.state.favorites);
+    const newFav = searchById(id,list);
+    const isAlreadyInFavs = searchById(id,favorites);
     
     if(newFav){ //Si se selecciona una tarjeta de busqueda
       if(!isAlreadyInFavs){ // Si no está en favoritos se añade
-        this.saveToLocalStorage('favorites', [...this.state.favorites,newFav])
-        this.setState({favorites: [...this.state.favorites,newFav] }); 
+        saveToLocalStorage('favorites', [...favorites,newFav])
+        setFavorites([...favorites,newFav]); 
       }else{ // Si ya se encuentra en favoritos, se borra
-        this.saveToLocalStorage('favorites',  this.state.favorites.filter(elem => elem !== newFav))
-        this.setState({favorites: this.state.favorites.filter(elem => elem !== newFav) }); 
+        saveToLocalStorage('favorites',  favorites.filter(elem => elem !== newFav))
+        setFavorites(favorites.filter(elem => elem !== newFav)); 
       }
     }else{ //Si se selecciona una tarjeta de favoritos
-        this.saveToLocalStorage('favorites', this.state.favorites.filter(elem => elem !== isAlreadyInFavs))
-        this.setState({favorites: this.state.favorites.filter(elem => elem !== isAlreadyInFavs) });  
+        saveToLocalStorage('favorites', favorites.filter(elem => elem !== isAlreadyInFavs))
+        setFavorites(favorites.filter(elem => elem !== isAlreadyInFavs));  
     }
   }
 
-  saveToLocalStorage(name, item){
+  const saveToLocalStorage = (name, item) => { 
     localStorage.setItem(name,  JSON.stringify(item));
   }
 
-  loadFromLocalStorage(name){
-    return JSON.parse(localStorage.getItem(name));
-  }
 
-  searchById(id,arr){
+  const searchById = (id,arr) => { 
     for(const elem of arr){
       if(elem && elem.imdbID.toString() === id.toString()){
         return elem;
@@ -88,6 +61,23 @@ class  App extends React.Component {
     }
     return null;
   }
+
+    return (
+      <div className="App">
+          <InputSearch buttonName={'busca'} actionToPerform={ getMovies }></InputSearch>
+          <main>
+            <aside className="aside">
+              <MovieList list={favorites} cardSelected={ cardSelected } class="favorite" ></MovieList>
+            </aside>
+            <section className="section">
+              <MovieList list={list}  cardSelected={ cardSelected } class="normal" ></MovieList>
+            </section>
+          </main>
+      </div>
+    );
+
+
+
 }
 
 export default App;
